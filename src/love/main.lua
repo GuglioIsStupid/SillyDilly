dba0b1776e00c954c39c0c43387e8d3af382e526
clothing-hanger <kelbymixon1@gmail.com>
2023-08-27 10:33:37 -0500

    t

--- src/love/main.lua
+++ src/love/main.lua
@@ -456,10 +456,6 @@ function love.load()
 	weeksAmusia = require "states.weeks.weeksAmusia"
 	weeksBrimstone = require "states.weeks.brimstone"
 	weeksPasta = require "states.weeks.weeksPasta"
-<<<<<<< HEAD
-
-=======
->>>>>>> 31c811aa988f2b78eceac68f67d47c50ab17cc5f
 
 	-- Load substates
 	gameOver = require "substates.game-over"
