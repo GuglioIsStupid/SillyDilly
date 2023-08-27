0de12a396b31862786eb50fc82f23e43e3cd9727
GuglioIsStupid <bradsbluetab@gmail.com>
2023-08-26 20:44:01 -0400

    huh

--- src/love/main.lua
+++ src/love/main.lua
@@ -455,7 +455,8 @@ function love.load()
 	weeksShitno = require "states.weeks.weeksShitno"
 	weeksAmusia = require "states.weeks.weeksAmusia"
 	weeksBrimstone = require "states.weeks.brimstone"
-	
+	weeksPasta = require "states.weeks.weeksPasta"
+
 
 	-- Load substates
 	gameOver = require "substates.game-over"
